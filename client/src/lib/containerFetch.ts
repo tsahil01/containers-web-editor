bd550aa47a52bc2eee2482531e9d7a9cb4c31793
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-07-19 10:56:37 +0530

    chore: Update Dockerfile for code-server image with Ubuntu base and add support for ubuntu-vscode image

--- client/src/lib/containerFetch.ts
+++ client/src/lib/containerFetch.ts
@@ -1,39 +1,36 @@
 export interface Container {
-    containerId: string;
-    internalPort: number;
-    externalPort: number;
-  }
+  containerId: string;
+  internalPort: number;
+  externalPort: number;
+}
 
 export async function fetchNewContainer(): Promise<Container> {
-    const image = "ubuntu-vscode-node";
-  
-    try {
-  
-      const response = await fetch("http://localhost:4000/api/new-container", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          image,
-          cmd: "",
-        }),
-      });
-      
-      if (!response.ok) {
-        throw new Error("Failed to fetch new container");
-      }
-      const data: Container = await response.json();
+  const image = "ubuntu-vscode";
+
+  try {
+    const response = await fetch("http://localhost:4000/api/new-container", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        image,
+        cmd: "",
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch new container");
+    }
+    const data: Container = await response.json();
     // console.log("Container Data:", data);
     return data;
-  
-    } catch (error) {
-      console.error("Error fetching new container:", error);
-      return {
-        containerId: "",
-        internalPort: 0,
-        externalPort: 0,
-      };
-    }
+  } catch (error) {
+    console.error("Error fetching new container:", error);
+    return {
+      containerId: "",
+      internalPort: 0,
+      externalPort: 0,
+    };
   }
-  
+}
